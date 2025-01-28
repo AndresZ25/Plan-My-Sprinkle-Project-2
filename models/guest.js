326e832d73fda67955c8b4baa6d97da88db57330
AndresZ25 <andres.zun25@gmail.com>
2025-01-27 19:21:33 -0500

    merge files

--- models/guest.js
+++ models/guest.js
@@ -1,41 +1,32 @@
-const { open } = require('sqlite');
-const sqlite3 = require('sqlite3');
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
 
-const dbPromise = open({
-  filename: './guests.db',
-  driver: sqlite3.Database
-});
+class Guest extends Model {}
 
+Guest.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    rsvp_status: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    modelName: 'Guest',
+    freezeTableName: true,
+    underscored: true,
+    timestamps: true,
+  }
+);
 
-async function getAllGuests() {
-  const db = await dbPromise;
-  return await db.all('SELECT * FROM guests');
-}
-
-async function addGuest(guest) {
-  const { name, email, phone_number, rsvp_status, gift } = guest;
-  const db = await dbPromise;
-  await db.run(
-    'INSERT INTO guests (name, email, phone_number, rsvp_status, gift) VALUES (?, ?, ?, ?, ?)',
-    [name, email, phone_number, rsvp_status, gift]
-  );
-}
-
-
-async function updateRSVPStatus(id, rsvp_status) {
-  const db = await dbPromise;
-  await db.run('UPDATE guests SET rsvp_status = ? WHERE id = ?', [rsvp_status, id]);
-}
-
-
-async function deleteGuest(id) {
-  const db = await dbPromise;
-  await db.run('DELETE FROM guests WHERE id = ?', [id]);
-}
-
-module.exports = {
-  getAllGuests,
-  addGuest,
-  updateRSVPStatus,
-  deleteGuest
-};
+module.exports = Guest;
